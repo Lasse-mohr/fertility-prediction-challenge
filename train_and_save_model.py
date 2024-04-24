597b5ab01ec334823a4904872a30a9f2a9ca4e6c
Lasse Mohr <lmmi@dtu.dk>
2024-04-24 15:55:01 +0200

    Changed joblib to be installed using pip instead og conda

---
+++ train_and_save_model.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+import submission
+import training
+
+# loading data (predictors)
+train = pd.read_csv("/Users/lmmi/PreFer/training_data/PreFer_train_data.csv", low_memory = False, nrows = 100)
+# loading the outcome
+outcome = pd.read_csv("/Users/lmmi/PreFer/training_data/PreFer_train_outcome.csv", nrows = 100)
+
+# preprocessing the data
+train_cleaned = submission.clean_df(train)
+
+# training and saving the model
+training.train_save_model(train_cleaned, outcome)
+
