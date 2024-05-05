7074fac1ea7078fcc4f420da396c7a0810a9cf77
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2024-05-05 18:04:42 -0400

    Update text2vec.py
    Annotations added

--- data_processing/encoding/text2vec.py
+++ data_processing/encoding/text2vec.py
@@ -10,11 +10,23 @@ class TextTransform(BaseEstimator):
     """
 
     def __init__(self, model_name: str = "distiluse-base-multilingual-cased-v1") -> None:
+        """
+        Args:
+            model_name: specification of the 'SenteneTransformer' model (glove is the fastest and the simplest)
+        """
         assert model_name in ["distiluse-base-multilingual-cased-v1", "LaBSE",
                               "average_word_embeddings_glove.6B.300d"], "Wrong model name!"
         self.model = SentenceTransformer(model_name)
 
     def _vectorize_column(self, column: pd.Series):
+        """
+        Input:
+            column: a single Pandas column (pd.Series)
+        Return: 
+                np.array of size [n_rows x 512]
+                empty and Null rows are assigned vector of 0s,
+                the rest are transformed by the 'SentenceTransformer' model
+        """
         column = column.values
         non_empty_rows = np.where((column != None) & (column != ""))
         embeddings = self.model.encode(column[non_empty_rows])
@@ -24,10 +36,22 @@ class TextTransform(BaseEstimator):
 
     def fit(self, column: pd.Series):
         """
-        Return: np.array of size [n_rows x 512]
-                empty and Null rows are assigned vector of 0s
+        Input:
+            column: a single Pandas column (pd.Series)
+        Return: 
+                np.array of size [n_rows x 512]
+                empty and Null rows are assigned vector of 0s,
+                the rest are transformed by the 'SentenceTransformer' model
         """
         return self._vectorize_column(column)
 
     def transform(self, column: pd.Series):
+        """
+        Input:
+            column: a single Pandas column (pd.Series)
+        Return: 
+                np.array of size [n_rows x 512]
+                empty and Null rows are assigned vector of 0s,
+                the rest are transformed by the 'SentenceTransformer' model
+        """
         return self._vectorize_column(column)
