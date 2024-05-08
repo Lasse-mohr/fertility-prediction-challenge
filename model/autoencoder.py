93c636166ce90bb0fdd75dd6d4cb04e58fd672ee
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2024-05-07 21:58:53 -0400

    Clean update

--- model/autoencoder.py
+++ model/autoencoder.py
@@ -10,64 +10,72 @@ class ConvEncoderLayer(nn.Module):
                  output_size: int,
                  kernel_size: int,
                  padding_len: int,
-                 pooling_size: int):
+                 pooling_size: int,
+                 dropout: float = 0.1):
         super(ConvEncoderLayer, self).__init__()
+
+        self.input_size = input_size
+        self.output_size = output_size
+        self.kernel_size = kernel_size
+        self.padding_len = padding_len
+        self.pooling_size = pooling_size
+
         self.layer = nn.Conv1d(
             in_channels=input_size,
             out_channels=output_size,
             kernel_size=kernel_size,
             padding=padding_len)
         self.act = nn.Mish()
+        self.dropout = nn.Dropout(p=dropout)
+        self.norm = nn.LazyInstanceNorm1d()
         self.pool = nn.MaxPool1d(pooling_size)
 
     def forward(self, x):
         x = self.layer(x)
         x = self.act(x)
+        x = self.norm(x)
+        x = self.dropout(x)
         x = self.pool(x)
-        print(x.shape)
+        # print(x.shape)
         return x
+
+    def get_output_shape(self, x):
+        sc = (x + 2 * self.padding_len - self.kernel_size) + 1
+        output = (sc - self.pooling_size)/self.pooling_size + 1
+        return int(output)
 
 
 class ConvDecoderLayer(nn.Module):
     def __init__(self, input_size: int,
                  output_size: int,
-                 conv_kernel_size: int,
-                 stride: int,
-                 normalize: bool = True,
+                 hidden_dim: int,
+                 kernel_size: int = 24,
+                 dilation: int = 4,
                  dropout: float = 0.1):
         super(ConvDecoderLayer, self).__init__()
-        self.layer = nn.ConvTranspose1d(
-            in_channels=input_size,
-            out_channels=output_size,
-            kernel_size=conv_kernel_size,
-            stride=stride,
-            padding=1,
-            output_padding=1)  # Ensure the output shape is correctly adjusted
+
+        self.layer = nn.ConvTranspose1d(in_channels=input_size,
+                                        out_channels=output_size,
+                                        kernel_size=kernel_size,
+                                        dilation=dilation)
         self.act = nn.Mish()
-
-        if normalize:
-            self.norm = nn.LazyInstanceNorm1d()
-        else:
-            self.norm = None
-
-        if dropout is not None:
-            self.dropout = nn.Dropout(p=dropout)
-        else:
-            self.dropout = None
+        self.dropout = nn.Dropout(p=dropout)
+        self.norm = nn.LazyInstanceNorm1d()
+        self.ff = nn.LazyLinear(hidden_dim)
 
     def forward(self, x):
         x = self.layer(x)
         x = self.act(x)
-        if self.norm is not None:
-            x = self.norm(x)
-        if self.dropout is not None:
-            x = self.dropout(x)
-        print(x.shape)
+        x = self.norm(x)
+        x = self.dropout(x)
+        x = self.ff(x)
+        # print(x.shape)
         return x
 
 
 class AutoEncoder(nn.Module):
     def __init__(self, vocab_size: int,
+                 sequence_len: int,
                  embedding_size: int,
                  encoding_size: int) -> None:
         super(AutoEncoder, self).__init__()
@@ -75,52 +83,55 @@ class AutoEncoder(nn.Module):
         self.vocab_size = vocab_size
         self.embedding_size = embedding_size
         self.encoding_size = encoding_size
+        self.sequence_len = sequence_len
 
         self.embedding = nn.Embedding(vocab_size, embedding_size)
-        self.conv_encoders = nn.Sequential(ConvEncoderLayer(input_size=embedding_size, output_size=256, conv_kernel_size=2, pool_kernel_size=2),
-                                           ConvEncoderLayer(
-                                               input_size=256, output_size=128, conv_kernel_size=3, pool_kernel_size=2),
-                                           ConvEncoderLayer(
-                                               input_size=128, output_size=64, conv_kernel_size=3, pool_kernel_size=2),
-                                           ConvEncoderLayer(input_size=64, output_size=8, conv_kernel_size=3, pool_kernel_size=2))
-        self.linear_encoder = nn.Sequential(nn.LazyLinear(out_features=encoding_size),
-                                            nn.Mish(),
-                                            nn.LayerNorm(encoding_size),
-                                            nn.Linear(encoding_size, encoding_size))  # I am using Lazy layer since it is non-trivial to calculate the output of convolutions
-        # the model will figure it out
-        # Decoder
-        self.linear_decoder = nn.Sequential(
-            # Prepare to reshape into [batch, channels, length]
-            nn.Linear(encoding_size, encoding_size * 4),
-            nn.Mish()
+        self.encoder = nn.Sequential(
+            ConvEncoderLayer(input_size=embedding_size, output_size=embedding_size //
+                             4, kernel_size=3, padding_len=1, pooling_size=4),
+            ConvEncoderLayer(input_size=embedding_size // 4, output_size=embedding_size //
+                             8, kernel_size=6, padding_len=1, pooling_size=4),
+            ConvEncoderLayer(input_size=embedding_size // 8, output_size=encoding_size,
+                             kernel_size=6, padding_len=1, pooling_size=4),
+            nn.Mish(),
+            nn.AdaptiveAvgPool1d(1)
         )
-        self.conv_decoders = nn.Sequential(
-            ConvDecoderLayer(input_size=self.encoding_size,
-                             output_size=64, conv_kernel_size=3, stride=2),
-            ConvDecoderLayer(input_size=64, output_size=128,
-                             conv_kernel_size=3, stride=2),
-            ConvDecoderLayer(input_size=128, output_size=256,
-                             conv_kernel_size=3, stride=2),
-            ConvDecoderLayer(
-                input_size=256, output_size=embedding_size, conv_kernel_size=6, stride=4)
+
+        self.decoder = nn.Sequential(
+            ConvDecoderLayer(input_size=encoding_size,
+                             output_size=embedding_size // 8,
+                             hidden_dim=self.get_encoder_shapes()[-1],
+                             kernel_size=24, dilation=4),
+            ConvDecoderLayer(input_size=embedding_size // 8,
+                             output_size=embedding_size // 4, kernel_size=24, dilation=4,
+                             hidden_dim=self.get_encoder_shapes()[-2]),
+            ConvDecoderLayer(input_size=embedding_size // 4,
+                             output_size=embedding_size, kernel_size=24, dilation=4,
+                             hidden_dim=self.get_encoder_shapes()[-3]
+                             ),
+            nn.Mish(),
+            nn.LazyLinear(sequence_len)
         )
+
+    def get_encoder_shapes(self):
+        s = self.sequence_len
+        out = []
+        for encoder in self.encoder[:3]:
+            out.append(encoder.get_output_shape(s))
+            s = out[-1]
+        return out
 
     def forward(self, x):
         x = self.embedding(x)
         x = x.permute(0, 2, 1)  # we need to switch the things around
         # significantly reduce the dimensionality while allowing for interactions between 2D dimensions
-        x = self.conv_encoders(x)
-        x = x.view(x.size(0), -1)  # flatten the input of convolutions
-        x = self.linear_encoder(x)
+        x = self.encoder(x)
 
         # Decoding
-        x = self.linear_decoder(x)
+        x = self.decoder(x)
         # Reshape to match the expected input for ConvTranspose1d
-        x = x.view(x.size(0), self.encoding_size, -1)
-        x = self.conv_decoders(x)
         # Switch dimensions back to [batch_size, seq_len, embedding_size]
         x = x.permute(0, 2, 1)
-
         return x
 
     def encode(self, x):
@@ -130,13 +141,13 @@ class AutoEncoder(nn.Module):
         x = self.embedding(x)
         x = x.permute(0, 2, 1)  # we need to switch the things around
         # significantly reduce the dimensionality while allowing for interactions between 2D dimensions
-        x = self.conv_encoders(x)
-        x = x.view(x.size(0), -1)  # flatten the input of convolutions
-        x = self.linear_encoder(x)
+        x = self.encoder(x).view(x.size(0), -1)
         return x
 
-
+################
 # ARCHIVE
+
+
 class _AutoEncoder(nn.Module):
     def __init__(self, num_embeddings: int,
                  encoding_dim: int = 16,
