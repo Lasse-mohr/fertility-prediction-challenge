e458b3166effb220dd847ac0cb03fe473b1d44f5
Germans Savcisens <38817889+carlomarxdk@users.noreply.github.com>
2024-05-10 10:20:23 -0400

    initialize constants via register_buffer

--- model/embeddings.py
+++ model/embeddings.py
@@ -4,11 +4,16 @@ import torch
 class SurveyEmbeddings(torch.nn.Module):
     def __init__(self, vocab_size: int, n_questions: int = None, n_years: int = 14, embedding_dim: int = 16):
         super().__init__()
-        self.answer_embedding = torch.nn.Embedding(vocab_size, embedding_dim) # Vocab size is number of unique answers
-        self.yearly_embedding = torch.nn.Embedding(n_years, embedding_dim) # 14 years of data
+        # Vocab size is number of unique answers
+        self.answer_embedding = torch.nn.Embedding(vocab_size, embedding_dim)
+        self.yearly_embedding = torch.nn.Embedding(
+            n_years, embedding_dim)  # 14 years of data
         if n_questions is not None:
-            self.question_embedding = torch.nn.Embedding(n_questions, embedding_dim) # Number of unique questions
-            self.question_range = torch.arange(n_questions) # Fixed range of questions
+            self.question_embedding = torch.nn.Embedding(
+                n_questions, embedding_dim)  # Number of unique questions
+            self.register_buffer(
+                "question_range", torch.arange(n_questions))  # )
+            # self.question_range = torch.arange(n_questions) # Fixed range of questions
 
     def forward(self, year, answer):
         answer = self.answer_embedding(answer)
